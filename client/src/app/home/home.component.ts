31de976f3a0368b502d661c72b27c7b9e76d3d83
suman osti <suman.osti401@gmail.com>
2023-09-14 21:12:46 +0545

    added photo gallery to client

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -10,23 +10,15 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => {},
-      complete: () => console.log('Request has completed'),
-    });
-  }
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;

3dbffe8d31a7e6d94b415dd76b81cf5c76049358
suman osti <suman.osti401@gmail.com>
2023-12-04 21:26:59 +0545

    added real time messaging functionality

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { TimeagoModule } from 'ngx-timeago';
-import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 import { FormsModule, NgForm } from '@angular/forms';
 
@@ -15,21 +14,17 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class MemberMessagesComponent implements OnInit{
   @ViewChild('messageForm') messageForm?: NgForm
   @Input() username?: string
-  @Input() messages: Message[] = [];
   messageContent = '';
 
-  constructor (private messageService: MessageService) {}
+  constructor (public messageService: MessageService) {}
 
   ngOnInit(): void {
   }
 
   sendMessage() {
     if (!this.username) return;
-    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
-      next: message => {
-        this.messages.push(message);
-        this.messageForm?.reset();
-      }
+    this.messageService.sendMessage(this.username, this.messageContent)?.then(() => {
+      this.messageForm?.reset();
     })
   }
 
